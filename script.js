539806202550eb94569a294fc2064e7ae626eba0
KariannaPaola <kariannaarriaga07@gmail.com>
2026-03-10 20:44:36 -0700

    act

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let contactos=[
   { nombre: "ana lopez", telefono: "+584143417985"},
   { nombre: "carlos oropeza", telefono: "+584143487245"},
-  { nombre: "maría barile", telefono: "+584143457975"},
+  { nombre: "maria barile", telefono: "+584143457975"},
   { nombre: "juan guerrero", telefono: "+584143419585"},
   { nombre: "marcos chirinos", telefono: "+584143413687"},
   { nombre: "ana ortega", telefono: "+584143415005"}
@@ -23,12 +23,16 @@ btnBuscar.addEventListener("click", () => {
   else if (contactosFiltrados.length>=1){
     div.innerHTML=""
     for(let contacto of contactosFiltrados){
-    const contactosMostrados=document.createElement("li")
-    contactosMostrados.innerHTML=contacto.nombre + " " + contacto.telefono
-    contactosMostrados.classList.add('estilos')
-    div.appendChild(contactosMostrados)
-    document.querySelector("#nombreIngresado").value=""
-    }
+      const contactosMostrados = document.createElement("li");
+      contactosMostrados.className =
+      "flex justify-between items-center bg-teal-50 border border-teal-200 px-4 py-2 rounded-lg shadow-sm hover:shadow-md transition";
+      contactosMostrados.innerHTML = `
+        <span class="font-semibold text-gray-800">${contacto.nombre}</span>
+        <span class="text-gray-600 text-sm">${contacto.telefono}</span>
+      `;
+      div.appendChild(contactosMostrados);
+        document.querySelector("#nombreIngresado").value=""
     }
   }
+}
 );
